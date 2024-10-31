f5976ca0df204cbcf5cc32ae3634528483262b9d
rishitha.menusha <rishitha.menush@gmail.com>
2024-10-31 20:44:20 +0530

    update the login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,106 +10,118 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.sports_gymnastics,
-                size: 100,
-              ),
-              //login animation
-              Text("Hello Again!",
-              style: GoogleFonts.bebasNeue(
-                fontSize: 54
-              ),),
-              const SizedBox(height: 10,),
-
-              const Text("Welcome back, you\'ve been missed!",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.sports_gymnastics,
+                  size: 100,
+                ),
+                //login animation
+                Text("Hello Again!",
+                style: GoogleFonts.bebasNeue(
+                  fontSize: 54
                 ),),
-
-              const SizedBox(height: 50,),
-
-              //email textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: const TextField(
+                const SizedBox(height: 10,),
+            
+                const Text("Welcome back, you\'ve been missed!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),),
+            
+                const SizedBox(height: 50,),
+            
+                //email text field
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    controller: _emailController,
                     decoration: InputDecoration(
-                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                       hintText: "Email",
-                      contentPadding: EdgeInsets.only(left: 15),
+                      fillColor: Colors.white,
+                      filled: true,
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20,),
-
-              //password Textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: const TextField(
+                const SizedBox(height: 20,),
+            
+                //password Text field
+            
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    controller: _passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
-                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                       hintText: "Password",
-                      contentPadding: EdgeInsets.only(left: 15),
+                      fillColor: Colors.white,
+                      filled: true,
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 15,),
-              //sign in button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(12)),
-                  child: const Center(
-                    child: Text("Sign In",
-                    style: TextStyle(color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),),
+            
+                const SizedBox(height: 15,),
+                //sign in button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12)),
+                    child: const Center(
+                      child: Text("Sign In",
+                      style: TextStyle(color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 25,),
-              //not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Forgot Password? ",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),),
-                  Text("Register Now",
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold
-                  ),),
-                ],
-              )
-            ],
+                const SizedBox(height: 25,),
+                //not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Forgot Password? ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text("Register Now",
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
