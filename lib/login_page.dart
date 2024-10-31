bdc7e1ddc3e9ee794426b4743f65ddfd89f36555
rishitha.menusha <rishitha.menush@gmail.com>
2024-10-31 20:28:25 +0530

    update the login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -104,7 +104,7 @@ class _LoginPageState extends State<LoginPage> {
                   ),),
                   Text("Register Now",
                   style: TextStyle(
-                    color: Colors.blue,
+                    color: Colors.deepPurple,
                     fontWeight: FontWeight.bold
                   ),),
                 ],
